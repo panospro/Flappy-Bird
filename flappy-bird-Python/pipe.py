bd6ed231fff2a5ee9443fa5b06a87ec4a037a79a
panospro <panagiotisskaki@yahoo.gr>
2023-03-08 20:11:50 +0200

    Import pipes

--- flappy-bird-Python/pipe.py
+++ flappy-bird-Python/pipe.py
@@ -1,69 +1,3 @@
-# # import pygame
-
-# # # set up game window
-# # WIDTH = 600
-# # HEIGHT = 800
-# # gravity = 0.25
-
-# # pygame.init()
-# # # pygame.mixer.init() // for sound
-# # # screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# # pygame.display.set_caption("Flappy Bird")
-
-# # # load pipe image
-# # pipe_img = pygame.image.load("flappy-bird-Python/images/fullPipeBottom.png").convert_alpha()
-
-# # # Define the pipe class
-# # class Pipe(pygame.sprite.Sprite):
-# #     def __init__(self, height):
-# #         pygame.sprite.Sprite.__init__(self)
-# #         self.image = pipe_img
-# #         self.rect = self.image.get_rect()
-# #         self.rect.x = WIDTH
-# #         self.rect.y = height
-# #         self.speed = -4
-    
-# #     def update(self):
-# #         self.rect.x += self.speed
-# #         if self.rect.right < 0:
-# #             self.kill()
-    
-# #     def get_height(self):
-# #         return self.rect.y + self.rect.height
-
-# import pygame
-
-# # set up game window
-# WIDTH = 600
-# HEIGHT = 800
-# gravity = 0.25
-
-# pygame.init()
-# # pygame.mixer.init() // for sound
-# # screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# pygame.display.set_caption("Flappy Bird")
-
-# # load pipe image
-# pipe_img = pygame.image.load("flappy-bird-Python/images/fullPipeBottom.png").convert_alpha()
-
-# # Define the pipe class
-# class Pipe(pygame.sprite.Sprite):
-#     def __init__(self, height):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.image = pipe_img
-#         self.rect = self.image.get_rect()
-#         self.rect.x = WIDTH
-#         self.rect.y = height
-#         self.speed = -4
-    
-#     def update(self):
-#         self.rect.x += self.speed
-#         if self.rect.right < 0:
-#             self.kill()
-    
-#     def get_height(self):
-#         return self.rect.y + self.rect.height
-
 import pygame
 import random
 
@@ -112,19 +46,25 @@ class PipePair(pygame.sprite.Group):
         self.pipe_frequency = 120
         self.score = 0
         self.pipes_passed = 0  # initialize number of pipes passed to 0
-    
+        self.last_spawn_time = pygame.time.get_ticks()  # initialize last spawn time
+        
     def spawn_pipes(self):
-        pipe_heights = [150, 200, 250, 300]
-        pipe_height = random.choice(pipe_heights)
-        top_pipe = Pipe(pipe_height - self.pipe_gap -650, True)
-        bottom_pipe = Pipe(pipe_height + self.pipe_gap, False)
+        top_pipe_height = random.randint(100, 400)  # choose a random height for the top pipe
+        bottom_pipe_height = top_pipe_height + self.pipe_gap  # calculate the height for the bottom pipe
+        top_pipe = Pipe(top_pipe_height - self.pipe_gap - 650, True)
+        bottom_pipe = Pipe(bottom_pipe_height, False)
         self.add(top_pipe, bottom_pipe)
         
         
     def update(self, bird):
         pygame.sprite.Group.update(self)
-        if len(self) < 2 and bird.rect.right >= self.pipe_frequency:
+
+        # Spawn pipes if the time elapsed since last spawn is greater than the desired frequency
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_spawn_time >= self.pipe_frequency * 12:
             self.spawn_pipes()
+            self.last_spawn_time = current_time
+            
         for pipe in self.sprites():
             if bird.rect.left > pipe.rect.right and not pipe.scored:
                 self.pipes_passed += 1
