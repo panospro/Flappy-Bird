aa71f0edcec402837e731aada648fb31bd3707c4
protops <65860101+panospro@users.noreply.github.com>
2023-12-22 15:47:35 +0200

    Update pipe.py

--- flappy-bird-Python/pipe.py
+++ flappy-bird-Python/pipe.py
@@ -28,20 +28,3 @@ class Pipe(pygame.sprite.Sprite):
     
     def get_height(self):
         return self.rect.bottom
-
-
-class Pipe2(pygame.sprite.Sprite):
-    def __init__(self, height, is_top):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pipe_img_top if is_top else pipe_img_bottom
-        self.rect = self.image.get_rect(x=WIDTH, y=height)
-        self.speed = -4
-        self.scored = False
-    
-    def update(self):
-        self.rect.x += self.speed
-        if self.rect.right < 0:
-            self.kill()
-    
-    def get_height(self):
-        return self.rect.bottom
